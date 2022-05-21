5c63f456ff111216ff3d9e21840c84659e1fc39c
Dan Tong <dan@dtong.nz>
2022-05-21 16:19:01 +1200

    freeze and reverse poc

--- echoes.lua
+++ echoes.lua
@@ -2,13 +2,17 @@
 --
 -- four tap clocked delay/looper
 
+s = require 'sequins'
 sc = softcut
+
 voices = 4
 zone_length = 60 -- buffer zone per voice
 pans = {-1.0, -0.5, 0.5, 1}
 default_pre = 0.5 -- TODO move to params
+voice_states = {}
 
 function init()
+  sc.buffer_clear()
   audio.level_cut(1)
   audio.level_adc_cut(1)
 
@@ -16,6 +20,14 @@ function init()
     init_softcut(v)
     init_params(v)
     clock.run(reset_head, v)
+
+    -- params for testing
+    if v == 1 then
+      sc.pan(v, pans[v])
+      sc.level(v, 0.8)
+      voice_states[v] = {}
+      voice_states[v].sfreeze = s{1,0}:every(v)
+    end
   end
 
   -- TODO enable this after setting sane defaults
@@ -36,6 +48,7 @@ function init_softcut(v)
   sc.enable(v, 1)
   sc.buffer(v, 1)
   sc.level(v, 0.2)
+  sc.fade_time(v, 0.5)
   sc.play(v, 1)
   sc.rate(v, 1.0)
   sc.pan(v, 0)
@@ -54,10 +67,6 @@ function init_softcut(v)
   sc.filter_bp(v, 1.0);
   sc.filter_hp(v, 0.0);
   sc.filter_rq(v, 2.0);
-
-  -- params for testing
-  sc.pan(v, pans[v])
-  sc.level(v, 0.8)
 end
 
 function init_params(v)
@@ -76,24 +85,28 @@ function init_params(v)
 
   params:add_binary(v.."freeze", "freeze (K3)", "toggle", 0)
   params:set_action(v.."freeze",function(x)
-    -- TODO handle rate changes when toggling freeze
     if x == 1 then
       sc.rec_level(v, 0)
-      sc.pre_level(v, 1.0) -- preserve current buffer contents
-    else
+      sc.pre_level(v, 1.0) -- freeze current buffer contents
+      sc.rate(v, -2)
+      -- sc.rate(v, params:get(v.."rate")) -- allows for pitch shifting when frozen
+    else -- record
       sc.rec_level(v, 1.0)
       sc.pre_level(v, default_pre)
+      sc.rate(v, 1)
+      -- sc.rate(v, 1.0)
     end
     _menu.rebuild_params()
   end)
 end
+
 function redraw()
   screen.clear()
 
-    -- screen.move(10, voices*10+10)
-    -- screen.text("rec position:")
-    -- screen.move(118,voices*10+10)
-    -- screen.text_right(string.format("%.2f", positions[5]))
+  -- screen.move(10, voices*10+10)
+  -- screen.text("rec position:")
+  -- screen.move(118,voices*10+10)
+  -- screen.text_right(string.format("%.2f", positions[5]))
 
   for v=1, voices do
     screen.move(10, v*10)
@@ -112,23 +125,30 @@ end
 
 function reset_head(v)
   while true do
-    -- TODO change playback rate
     clock.sync(v)
-    -- TODO calculate correctly depending on rate etc
-    -- if params:get(v.."rate") < 0 then
-    --   position = vbuffer * v -- playing in reverse, start at end of buffer zone
-    -- else
-    --   position = vbuffer * v - vbuffer -- start at start of buffer zone
-    -- end
-    sc.position(v, (v-1) * zone_length)
+    local zone_start = (v-1) * zone_length
+
+    if params:get(v.."freeze") then--and params:get(v.."rate") < 0 then
+      -- frozen and playing in reverse, start at end of buffer zone
+      local position = zone_start + v/(clock.get_tempo()/60) -- TODO don't use 'v' for clock.sync
+      sc.position(v, position)
+    else
+      -- start at start of buffer zone
+      sc.position(v, zone_start)
+    end
   end
 end
 
 function toggle_freeze()
   print("toggle freeze")
   for v=1, voices do
-    local state = params:get(v.."freeze")
-    params:set(v.."freeze", math.abs(state-1))
+    voice_states[v].sfreeze()
+    -- peek is a workaround because sfreeze() doesn't always return a number
+    local state = voice_states[v].sfreeze:peek()
+    -- local state = math.abs(params:get(v.."freeze")-1) -- flip between zero and one
+    -- TODO for this to work, need to fix loop start/end based on rate
+    -- if state == 1 then flip_rate(v) end
+    params:set(v.."freeze", state)
   end
 
   clock.run(function()
@@ -140,10 +160,13 @@ function toggle_freeze()
   end)
 end
 
-function flip_rate()
-  for v=1, voices do
-    local rate = params:get(v.."rate")
-    sc.rate(v, -rate)
-  end
+function flip_rate(v)
+  -- for v=1, voices do
+  local rate = params:get(v.."rate")
+  sc.rate(v, -rate)
+  -- end
 end
 
+function r()
+  norns.script.load(norns.state.script)
+end
