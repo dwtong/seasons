213c5aad4e6f83af1fc95ab5c4114e51bb3aecf6
Dan Tong <dan@dtong.nz>
2022-05-23 10:32:54 +1200

    Add preroll time, fix zone length

--- lib/voice.lua
+++ lib/voice.lua
@@ -1,31 +1,33 @@
-ZONE_LENGTH = 60 -- buffer zone per voice
-DEFAULT_PRE = 0.5 -- TODO move to params
+ZONE_LENGTH = 64 -- buffer zone per voice, factor of 2
+ROLL_LENGTH = 8 -- pre/post roll at start and end of zones
+DEFAULT_PRE = 0.0 -- TODO move to params
 
 voice = {}
 
-  -- v.clock_sync = 1/voice -- TODO move to params
-function voice.zone_start(v) return (v-1) * ZONE_LENGTH end
-function voice.zone_end(v) return v * ZONE_LENGTH end
+function voice.zone_start(v) return (v-1)*ZONE_LENGTH + v*ROLL_LENGTH end
+function voice.zone_end(v) return v*ZONE_LENGTH + v*ROLL_LENGTH  end
 
 function voice.init_softcut(v)
-  -- TODO custom routing
   sc.level_input_cut(1, v, 1.0)
   sc.level_input_cut(2, v, 1.0)
 
   sc.enable(v, 1)
   sc.buffer(v, 1)
-  sc.level(v, 0.0)
   sc.fade_time(v, 0.5)
   sc.play(v, 1)
+
+  -- TODO set these with params:bang
+  sc.level(v, 0.0)
   sc.rate(v, 1.0)
   sc.pan(v, 0)
-  sc.loop(v, 1)
-  sc.position(v, voice.zone_start(v))
-  sc.loop_start(v, voice.zone_end(v))
-  sc.loop_end(v, voice.zone_end(v))
-  sc.rec(v, 1)
   sc.rec_level(v, 1.0)
   sc.pre_level(v, DEFAULT_PRE)
+
+  sc.loop(v, 1)
+  sc.position(v, voice.zone_start(v))
+  sc.loop_start(v, voice.zone_start(v))
+  sc.loop_end(v, voice.zone_end(v))
+  sc.rec(v, 1)
 
   -- nice filter defaults from halfsecond
   sc.filter_dry(v, 0.125);
@@ -43,7 +45,6 @@ function voice.init_params(v)
   params:add_control(v.."pan", v.." pan", controlspec.PAN)
   params:set_action(v.."pan", function(n) sc.pan(v, n) end)
 
-  -- TODO add sane rates
   -- params:add_option(v.."rate", v.." rate", rates, 10)
   params:add_control(v.."rate", v.." rate", controlspec.PAN)
   params:set_action(v.."rate", function(n) sc.rate(v, n) end)
@@ -66,7 +67,8 @@ function voice.init_params(v)
       -- TODO reenable after adding rate params
       -- sc.rate(v, params:get(v.."rate")) -- allows for pitch shifting when playing back
     end
-    _menu.rebuild_params()
+    -- TODO only call this when actually viewing the menu
+    -- _menu.rebuild_params()
   end)
 end
 
