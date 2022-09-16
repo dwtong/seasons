31f702c884a347e175000f702de5a0380e65610b
Dan Tong <dan@dtong.nz>
2022-09-17 11:19:32 +1200

    Toggle all rec with alt button on grid

--- lib/grid.lua
+++ lib/grid.lua
@@ -1,4 +1,5 @@
 _grid = {}
+_grid.dirty = true
 g = grid.connect()
 
 local toggled = {}
@@ -10,18 +11,23 @@ for x=1,16 do
   end
 end
 
-callbacks = {}
+local callbacks = {}
 for x=1,16 do callbacks[x] = {} end
 
-_grid.dirty = true
+local alt = false
 
 function _grid.init()
   clock.run(grid_redraw_clock)
 
   for v=1,4 do
     x = (v-1)*4+1
-    -- TODO use actions instead of setting params directly
-    callbacks[x][2] = function(state) params:set(v.."togglerec", state) end
+    callbacks[x][2] = function(state)
+      if alt then
+        actions.toggle_rec()
+      else
+        actions.toggle_rec(v)
+      end
+    end
   end
 end
 
@@ -51,10 +57,20 @@ function grid_redraw()
   -- rec
   for v=1,4 do
     x = (v-1)*4+1
-    brt = 4
-    if voice.is_rec(v) then brt = 10 end
+    local brt = voice.is_rec(v) and 10 or 6
     g:led(x, 2, brt)
   end
+
+  -- mutes
+  for v=1,4 do
+    x = (v-1)*4+2
+    local brt = 6
+    g:led(x, 2, brt)
+  end
+
+
+  local brt = alt and 8 or 4
+  g:led(1, 8, brt)
 
   g:refresh()
 end
@@ -68,12 +84,16 @@ function toggle(x, y)
 end
 
 function g.key(x, y, z)
-    print(x..' '..y..' '..z)
+  -- print(x..' '..y..' '..z)
+
+  if x == 1 and y == 8 then alt = (z == 1) end
 
   if z == 1 then
     state = toggle(x, y)
     if callbacks[x][y] then callbacks[x][y](state) end
   end
+
+  _grid.dirty = true
 end
 
 return _grid
